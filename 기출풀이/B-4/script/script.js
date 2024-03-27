f7cf81901ce29bb55b108f1c02f6db0a53ade1e7
tyeebang <111326879+tyeebang@users.noreply.github.com>
2024-03-27 16:00:00 +0900

    기출문제 C-1

--- 기출풀이/B-4/script/script.js
+++ 기출풀이/B-4/script/script.js
@@ -2,6 +2,8 @@ window.onload = function() {
     $('.notice-list > a:first-child').click(function() {
         $('.pop').css('display', 'flex')
     })
+
+
     $('.pop-inner button').click(function() {
         $('.pop').css('display', 'none')
     })
